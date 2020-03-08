a08e346f00015bb3ee5e6a6a31322f6452b8c6e2
Ejones820 <ejones129@gmail.com>
2020-03-08 19:53:14 +0000

    Update osm_validator.py
    Fetchs user's Bing API key from a local file - namely bing_api_key.txt

--- flask_ui/osm_validator.py
+++ flask_ui/osm_validator.py
@@ -9,6 +9,7 @@ A Flask UI to semi-automate the process of validating OSM PV groupings.
 
 import os
 import pickle
+import warnings
 from flask import Flask, request, url_for, redirect
 from flask.templating import render_template
 import pandas as pd
@@ -37,11 +38,12 @@ def validate_osm_groups(group_id):
     else:
         with open(groups_cache_file, "rb") as fid:
             osm_groups = pickle.load(fid)
-    if group_id not in osm_groups.id:
-        return home_page()
+    if group_id > osm_groups.id.max():
+        return redirect(url_for("home_page"))
     if "is_valid" in request.form:
         is_valid = request.form["is_valid"] == "yes"
-        flush_results(group_id, is_valid)
+        validation_notes = request.form["validation_notes"]
+        flush_results(group_id, is_valid, validation_notes)
         return redirect(url_for("validate_osm_groups", group_id=group_id+1))
     group = osm_groups.loc[osm_groups.id == group_id]
     ways = {}
@@ -56,11 +58,12 @@ def validate_osm_groups(group_id):
     mean_lats = np.mean(mean_lats)
     mean_lons = np.mean(mean_lons)
     return render_template("validate_osm_groups.html", ways=ways, group_id=group_id,
-                           center_lat=mean_lats, center_lon=mean_lons)
+                           center_lat=mean_lats, center_lon=mean_lons, bing_key=bing_key)
 
-def flush_results(group_id, is_valid):
+def flush_results(group_id, is_valid, validation_notes):
     results_file = os.path.join(ROOT_PATH, "results", "group_validations.csv")
-    new_result = pd.DataFrame([[group_id, is_valid]], columns=["group_id", "is_valid"])
+    new_result = pd.DataFrame([[group_id, is_valid, validation_notes]], columns=["group_id", 
+                                                                    "is_valid", "validation_notes"])
     if os.path.isfile(results_file):
         results = pd.read_csv(results_file)
         if group_id in results.group_id:
@@ -69,3 +72,15 @@ def flush_results(group_id, is_valid):
     else:
         results = new_result
     results.to_csv(results_file, index=False)
+
+def load_bing_key(api_key_file):
+    try:
+        with open(api_key_file) as fid:
+            key = fid.read().strip()
+    except FileNotFoundError:
+        warnings.warn("Failed to load Bing Maps API key - you will not be able to make new "
+                      "queries to the Bing Maps API!")
+        return None
+    return key
+api_key_file = os.path.join(ROOT_PATH, "bing_api_key.txt")
+bing_key = load_bing_key(api_key_file)
